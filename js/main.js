1796a4992f96421ba3df4d1ab86c6478fe1d29ea
vonSch4 <115642881+vonSch4@users.noreply.github.com>
2023-01-03 21:29:52 +0500

    some edits

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const hintContainer = document.querySelector(".hint-container");
+const addedItems = document.querySelector(".added-items");
 const input = document.querySelector("#search");
 
 function debounce(callback, ms) {
@@ -11,12 +13,42 @@ function debounce(callback, ms) {
 }
 
 function getValue() {
-  console.log(input.value);
-  return input.value;
+  return Promise.resolve().then(() => {
+    const value = input.value;
+    if (value) {
+      fetch(`https://api.github.com/search/repositories?q=${value}&per_page=5`)
+        .then((responce) => responce.json())
+        .then((result) => {
+          createHintCard(result.items);
+        })
+        .catch(console.log);
+    } else {
+      hintContainer.innerHTML = "";
+    }
+  });
+}
+
+function createHintCard(responce) {
+  const fragment = document.createDocumentFragment();
+  hintContainer.innerHTML = "";
+
+  responce.forEach((item) => {
+    const card = document.createElement("div");
+    card.classList.add("hint-card");
+    card.textContent = item.name;
+    fragment.append(card);
+  });
+
+  hintContainer.append(fragment);
 }
 
 const debouncedGetValue = debounce(getValue, 500);
 
 input.addEventListener("input", () => {
   debouncedGetValue();
+});
+
+hintContainer.addEventListener("click", (evt) => {
+  const target = evt.target;
+  console.dir(target);
 });
