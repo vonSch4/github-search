6894d19260031136fcf556e4b621f2a67c4f6980
vonSch4 <115642881+vonSch4@users.noreply.github.com>
2023-01-03 23:27:26 +0500

    some edits

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
 const hintContainer = document.querySelector(".hint-container");
-const addedItems = document.querySelector(".added-items");
+const addedCardsContainer = document.querySelector(".added-cards-container");
 const input = document.querySelector("#search");
+
+const debouncedGetValue = debounce(getValue, 600);
 
 function debounce(callback, ms) {
   let timer;
+
   return function (...args) {
     clearTimeout(timer);
-    timer = setTimeout(() => {
-      callback.call(this, ...args);
-    }, ms);
+
+    return new Promise((resolve) => {
+      timer = setTimeout(() => {
+        resolve(callback.call(this, ...args));
+      }, ms);
+    });
   };
 }
 
 function getValue() {
-  return Promise.resolve().then(() => {
+  return new Promise((resolve) => {
     const value = input.value;
+
     if (value) {
       fetch(`https://api.github.com/search/repositories?q=${value}&per_page=5`)
         .then((responce) => responce.json())
         .then((result) => {
           createHintCard(result.items);
+          resolve(result.items);
         })
         .catch(console.log);
     } else {
@@ -36,19 +44,32 @@ function createHintCard(responce) {
     const card = document.createElement("div");
     card.classList.add("hint-card");
     card.textContent = item.name;
+    card.dataset.owner = item.owner.login;
+    card.dataset.stars = item.stargazers_count;
     fragment.append(card);
   });
 
   hintContainer.append(fragment);
 }
 
-const debouncedGetValue = debounce(getValue, 500);
+function addCard(card) {
+  addedCardsContainer.insertAdjacentHTML(
+    "afterbegin",
+    `<div class='added-card'>
+      <p>Name: ${card.innerHTML}</p>
+      <p>Owner: ${card.dataset.owner}</p>
+      <p>Stars: ${card.dataset.stars}</p>
+    </div>
+    `
+  );
+  input.value = "";
+  hintContainer.innerHTML = "";
+}
 
 input.addEventListener("input", () => {
   debouncedGetValue();
 });
 
 hintContainer.addEventListener("click", (evt) => {
-  const target = evt.target;
-  console.dir(target);
+  addCard(evt.target);
 });
